556224b184a828179bbc3db590efbcfcba48b76e
Hemant Kapoor <kapoor.hemant@gmail.com>
2019-09-19 19:54:05 +0100

    Initial creation

---
+++ README.md
@@ -0,0 +1,35 @@
+
+# Simple Zigbee Test
+
+Simple Zigbee Test is an application to test "Simple Zigbee" ... Surprise...
+
+
+## Installation
+
+```
+git clone --recursive https://github.com/hemantkapoor/simpleZigbeeTest.git
+```
+
+##Compilation
+
+This is a cmake project.
+
+Simply run following commands
+
+```bash
+cmake .
+make
+```
+
+
+## TEST
+
+TDB
+
+
+## Contributing
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
+
---
+++ src/simpleZigbeeTest.cpp
@@ -0,0 +1,77 @@
+//============================================================================
+// Name        : simpleZigbeeTest.cpp
+// Author      : Hemant Kapoor
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+#include <memory>
+#include <thread>
+#include <vector>
+#include <future>
+#include"simpleSerial/comms/Comms.h"
+#include"simpleSerial/callback/BaseCallback.h"
+#include"simpleSerial/utility/Utility.h"
+
+
+char GetUserOption()
+{
+    char option;
+    std::cin.get(option);
+    return option;
+}
+
+class testSerialCallback : public SimpleSerialName::BaseCallback
+{
+public:
+	testSerialCallback() = default;
+	void callback(std::vector<uint8_t>& data) override
+	{
+		std::cout<<__PRETTY_FUNCTION__<< " : " << SimpleSerialName::Utility::hexStr(data.data(),data.size())<<std::endl;
+	}
+};
+
+int main() {
+	std::cout << "!!!Hello World!!!" << std::endl; // prints !!!Hello World!!!
+	std::string serialPath =  R"(/dev/pts/19)";
+	auto sp = std::make_shared<SimpleSerialName::Comms>(serialPath);
+	if(sp->startComms() == false)
+	{
+		std::cout<<__PRETTY_FUNCTION__<<" : Cannot access serial port\r\n";
+		return -1;
+	}
+
+	//Lets add a callback
+	auto myCallback = std::make_shared<testSerialCallback>();
+	sp->addCallback(myCallback);
+	//If here then we better spawn a thread
+	std::thread thread_1(&SimpleSerialName::Comms::processRead, sp);
+	std::cout<<__PRETTY_FUNCTION__<<" : Serial port Running... Press q to quit\r\n";
+	char option;
+
+	std::vector<uint8_t> transmitData{'H','E','L','L','O','\r','\n'};
+
+	auto future = std::async(std::launch::async, GetUserOption);
+
+	while(option != 'q')
+	{
+		if (future.wait_for(std::chrono::seconds(0)) == std::future_status::ready)
+		{
+			option = future.get();
+		}
+		//For test purpose lets write
+		if(sp->transmitData(transmitData) == false)
+		{
+			std::cout<<__PRETTY_FUNCTION__<<" : Transmit Failed\r\n";
+		}
+		std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+	}
+	//We are here so do graceful exit
+	std::cout<<__PRETTY_FUNCTION__<<" : Closing Application\r\n";
+	sp->stopRead();
+	thread_1.join();
+
+	return 0;
+}
