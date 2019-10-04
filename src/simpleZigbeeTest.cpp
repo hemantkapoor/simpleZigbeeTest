a2d894d9a9f0a1b07cd7d5d15a2bfd33165fd4e6
Hemant Kapoor <kapoor.hemant@gmail.com>
2019-10-04 14:55:31 +0100

    Skeleton ZCL Test

--- src/simpleZigbeeTest.cpp
+++ src/simpleZigbeeTest.cpp
@@ -13,9 +13,13 @@
 #include <future>
 #include <experimental/filesystem>
 #include <string>
+#include <variant>
 #include "simpleDebug/SimpleDebug.h"
 #include"simpleSerial/comms/Comms.h"
 #include "simpleZigbee/manager/Manager.h"
+#include "simpleZigbee/zclHelper/ZclHelper.h"
+#include <variant>
+#include <cassert>
 
 namespace fs = std::experimental::filesystem;
 
@@ -30,6 +34,22 @@ char GetUserOption()
 
 int main()
 {
+
+	std::vector<uint8_t> blah{ 0x1E ,0x54 ,0x52 ,0x41 ,0x44 ,0x46 ,0x52 ,0x49 ,0x20 ,0x62 ,0x75 ,0x6C ,0x62 ,0x20 ,0x45 ,0x31 ,0x34 ,0x20 ,0x57 ,0x20 ,0x6F ,0x70 ,0x2F ,0x63 ,0x68 ,0x20 ,0x34 ,0x30 ,0x30 ,0x6C ,0x6D};
+	SimpleZigbeeName::ZclHelper zcl;
+	auto v = zcl.getZclData(SimpleZigbeeName::ZCL_ARRAY, blah.data());
+	try
+	{
+		auto val = std::get<std::string>(v);
+		std::cout << val;
+	}
+	catch (const std::bad_variant_access&)
+	{
+		std::cout << "Incorrect Conversion";
+	}
+	return 0;
+
+#ifdef NEVER
 	//Lets get current directory
 	auto pwd = fs::current_path();
 	auto newDir = pwd / "debugLog";
@@ -74,4 +94,5 @@ int main()
 	//We are here so do graceful exit
 	m_debug->log(SimpleDebugName::LOG, std::string(__PRETTY_FUNCTION__) + " : Closing Application\r\n");
 	return 0;
+#endif
 }
